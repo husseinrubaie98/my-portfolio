027b54ccd0028209e0ddd0a19daebf488c29185f
husseinrubaie98 <77251920+husseinrubaie98@users.noreply.github.com>
2025-07-17 23:47:55 +0300

    Fix navbar dynamic active and scroll

--- src/app/navbar/navbar-item/navbar-item.component.ts
+++ src/app/navbar/navbar-item/navbar-item.component.ts
@@ -1,14 +1,23 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-item',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar-item.component.html',
   styleUrl: './navbar-item.component.css'
 })
 export class NavbarItemComponent {
 
-  @Input()
-  public itemName: String | undefined;
+  @Input() itemName: string ='';
+  @Input() isActive: boolean = false;
+  @Input() sectionId: string = '';
+
+  scrollToSection(sectionId: string) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
