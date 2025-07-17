89ce61beee37e0a2bc5a7f1d9aa07957d4f85483
husseinrubaie98 <77251920+husseinrubaie98@users.noreply.github.com>
2025-07-18 00:10:17 +0300

    Services section and missing files to commit

--- src/app/services-section/services-section.component.ts
+++ src/app/services-section/services-section.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { ServiceCardComponent } from "./service-card/service-card.component";
 import { WebDevelopmentIconComponent } from "../icons/web-development-icon/web-development-icon.component";
-import { CloudIconComponent } from "../icons/cloud-icon/cloud-icon.component";
-import { DatabaseIconComponent } from "../icons/database-icon/database-icon.component";
-import { MobileIconComponent } from "../icons/mobile-icon/mobile-icon.component";
-import { SecurityIconComponent } from "../icons/security-icon/security-icon.component";
 import { MaintenanceIconComponent } from "../icons/maintenance-icon/maintenance-icon.component";
+import { Service } from './models/Service';
+import { GearIconComponent } from '../icons/gear-icon/gear-icon.component';
+import { BugIconComponent } from '../icons/bug-icon/bug-icon.component';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-services-section',
   standalone: true,
-  imports: [ServiceCardComponent, WebDevelopmentIconComponent, CloudIconComponent, DatabaseIconComponent, MobileIconComponent, SecurityIconComponent, MaintenanceIconComponent],
+  imports: [ServiceCardComponent, WebDevelopmentIconComponent, MaintenanceIconComponent, NgComponentOutlet],
   templateUrl: './services-section.component.html',
   styleUrl: './services-section.component.css'
 })
@@ -22,4 +22,28 @@ export class ServicesSectionComponent {
       class="lucide lucide-code h-8 w-8 text-indigo-600">
       <polyline points="16 18 22 12 16 6"></polyline>
       <polyline points="8 6 2 12 8 18"></polyline></svg>`;
+
+  public services: Service[] = [
+    {
+      name: 'Web Development',
+      description: 'Building responsive and dynamic websites using Angular, Tailwindcss and Spring Boot or Django.',
+      icon: 'webDevelopment'
+    },
+    {
+      name: 'Automated Testing',
+      description: 'Implementing automated testing solutions to ensure code quality and reliability.',
+      icon: 'automatedTesting'
+    },
+    {
+      name: 'QA Engineering',
+      description: 'Providing quality assurance services to ensure software meets the highest standards.',
+      icon: 'softwareTesting'
+    }
+  ];
+
+  iconMap: { [key: string]: any } = {
+      webDevelopment: WebDevelopmentIconComponent,
+      automatedTesting: GearIconComponent,
+      softwareTesting: BugIconComponent
+    }
 }
