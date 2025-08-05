d47442ec06070026de487eaa75839fb8da6119f1
husseinrubaie98 <77251920+husseinrubaie98@users.noreply.github.com>
2025-08-05 03:49:46 +0300

    Fix button links in hero section

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -10,6 +10,12 @@ import { LinkedinButtonComponent } from "../buttons/linkedin-button/linkedin-but
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  goToLinkedIn() {
+    window.open('https://www.linkedin.com/in/husseinal-rubaie', '_blank');
+  }
+  goToGithub() {
+    window.open('https://github.com/husseinrubaie98/', '_blank');
+  }
 
   public bio: string = `Experienced Senior Software Engineer with over 5 years in the field, and a very wide skillset.
   Delivering innovative and efficient solutions across various technologies.
